527b7d14866fe945e08ea60b9f1b7fcd1adaabbe
St1ch <st1ch@mail.ru>
2018-01-11 23:14:16 +0300

    result form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 ;
-// Начинать писать отсюда!!!!
-
 $(document).ready(function(){
 
+	$(".fname").on('change', function(){
+		var str = this.value;
+		return $(".text").html("Привет " + str) 
+	});
 
+	$(".btn_submit").on("click", function(e){
+		e.preventDefault();
+		$('.popup-wrap').toggleClass("hidden");
+	});
+
+	$('.popup__close').on('click', function(){
+		$('.popup-wrap').toggleClass("hidden");
+	})
+	$('.popup-wrap').on('click', function(e){
+		if(e.target.className === 'popup-wrap'){
+			$('.popup-wrap').toggleClass("hidden");
+		}
+	})
 });
